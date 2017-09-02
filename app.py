4d225411da37ba36541e2806f388a737242f9050
James Zuccollo <j.zuccollo@hefce.ac.uk>
2017-09-02 12:40:53 +0100

    Add working interactivity with dropdown connected to chart.

--- app.py
+++ app.py
@@ -1,68 +1,60 @@
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
-import os
 
 app = dash.Dash(__name__)
 server = app.server
 server.secret_key = os.environ.get('SECRET_KEY', 'my-secret-key')
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/'
-                 'datasets/master/gapminderDataFiveYear.csv')
+# retrieve data
+temp_df = pd.DataFrame(
+    data={
+        'Bristol': [4, 3, 2, 4],
+        'London': [5, 3, 3, 6],
+        'Auckland': [8, 4, 7, 9],
+        'Wellington': [10, 9, 6, 8]
+    },
+    index=['Q1', 'Q2', 'Q3', 'Q4'])
 
+# construct layout
+app.title = "Climate comparison"
 app.layout = html.Div([
-    dcc.Graph(
-        id='graph-with-slider', animate=True), dcc.Slider(
-            id='year-slider',
-            min=df['year'].min(),
-            max=df['year'].max(),
-            value=df['year'].min(),
-            step=None,
-            marks={str(year): str(year)
-                   for year in df['year'].unique()})
+    html.H1("Climate comparison"),
+    dcc.Markdown("Select cities to compare their climates."), dcc.Dropdown(
+        id='city-select',
+        options=[{
+            'label': k,
+            'value': k
+        } for k in temp_df.columns],
+        value=['Bristol', 'London'],
+        multi=True), dcc.Graph(id='graphs')
 ])
 
 
+# create charts from inputs
 @app.callback(
-    dash.dependencies.Output('graph-with-slider', 'figure'),
-    [dash.dependencies.Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
+    dash.dependencies.Output('graphs', 'figure'),
+    [dash.dependencies.Input('city-select', 'value')])
+def update_figure(cities):
     traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(
-            go.Scatter(
-                x=df_by_continent['gdpPercap'],
-                y=df_by_continent['lifeExp'],
-                text=df_by_continent['country'],
-                mode='markers',
-                opacity=0.7,
-                marker={'size': 15,
-                        'line': {
-                            'width': 0.5,
-                            'color': 'white'
-                        }},
-                name=i))
+    for city in cities:
+        traces.append(go.Scatter(x=temp_df.index, y=temp_df[city], name=city))
+    layout = go.Layout(title="Temperature")
 
-    return {
-        'data': traces,
-        'layout': go.Layout(
-            xaxis={'type': 'log',
-                   'title': 'GDP Per Capita'},
-            yaxis={'title': 'Life Expectancy',
-                   'range': [20, 90]},
-            margin={'l': 40,
-                    'b': 40,
-                    't': 10,
-                    'r': 10},
-            legend={'x': 0,
-                    'y': 1},
-            hovermode='closest')
-    }
+    figure = go.Figure(data=go.Data(traces), layout=layout)
+    return figure
 
+
+external_css = [
+    "https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i",
+    "https://cdn.rawgit.com/plotly/dash-app-stylesheets/2cc54b8c03f4126569a3440aae611bbef1d7a5dd/stylesheet.css"
+]
+
+for css in external_css:
+    app.css.append_css({"external_url": css})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
